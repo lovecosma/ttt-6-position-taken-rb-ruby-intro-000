89a9884b1dfbb12bd5162e793f012ff035d49c00
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-18 02:00:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken? (board[index])
+  if index == "" || index == " "
+    false
+  elsif index == nil
+    false 
+  elsif index == "X" || index == "O"
+    
